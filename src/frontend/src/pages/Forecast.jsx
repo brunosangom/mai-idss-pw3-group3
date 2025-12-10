35aa14bef18df52615323225fffeb3030fef5b43
Jan <jan.albers@tutanota.com>
2025-12-10 20:17:52 +0100

    add map to forecast

--- src/frontend/src/pages/Forecast.jsx
+++ src/frontend/src/pages/Forecast.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { Grid } from '@mui/material';
 import Card from '../components/Card';
 import '../components/Content.css';
+import Map from '../components/Map';
 
 function Forecast() {
   return (
     <Grid container spacing={2}>
-      <Grid item xs={12} sx={{ display: 'block' }}>
-        <Card title="Forecast - Fire Weather Index" />
+      <Grid item xs={12}>
+        <Card title="Forecast - Fire Weather Index">
+          <div className="map-slot">
+            <Map wide={true} />
+          </div>
+        </Card>
       </Grid>
     </Grid>
   );
